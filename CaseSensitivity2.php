18d7a965a1d3b3d50e775fda610ec834f9612ecc
Freddy875 <fer875nandosm@gmail.com>
2020-12-12 13:08:27 -0600

    Recordad que PHP es case sensitivity

--- CaseSensitivity2.php
+++ CaseSensitivity2.php
@@ -11,8 +11,10 @@
 
         $color = "red";
         echo "My car is ". $color. "<br>";
-        echo "My house is ". $COLOR. "<br>";
-        echo "My boat is" . $coLOR. "<br>";
+        echo "My house is ". $color. "<br>";
+        echo "My boat is " . $color. "<br>";
+        //echo "My house is ". $COLOR. "<br>";
+        //echo "My boat is" . $coLOR. "<br>";
 
     ?>
 

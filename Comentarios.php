520cfd20cacf0446c0b31c5793dd5061359bbb7a
Freddy875 <fer875nandosm@gmail.com>
2020-12-14 11:56:17 -0600

    Aprendimos a hacer comentarios en PHP

---
+++ Comentarios.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <?php
+        //Esto es un comentario en una sola linea
+
+        #Esto tambien es un comentario en una sola linea
+
+        echo "Hola mundo"
+
+        /* 
+        Esto es un bloque de comentarios
+        en multiples lineas 
+        que avarca varias lineas
+        */
+
+        /*Tambien se pueden usar comentarios para dejar fuera partes de las lineas de codigo */
+        $x = 5 /* + 15*/ + 5;
+
+    ?>
+
+</body>
+
+</html>

0e140358afb60cdf5bc8fdc212221c83d4110106
Freddy875 <fer875nandosm@gmail.com>
2020-12-31 13:44:38 -0600

    Aprendimos a usar get y post para los formularios

---
+++ Formularios/Bienvenido_post.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    Bienvenido <?php echo $_POST["nombre"]; ?><br>
+    Tu direccion email es: <?php echo $_POST["email"]; ?>
+<body>
+</html>
---
+++ Formularios/FormularioSimple_get.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="Bienvenido_get.php" method="get">
+    Nombre: <input type="text" name="nombre"><br>
+    E-Mail: <input type="text" name="email"><br>
+    <input type="submit">
+    </form>
+</body>
+</html>
